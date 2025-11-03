9ccd1bf9718bac16da7705b0cceef3e2931140f0
Anurag Kumar <anuragakn18@gmail.com>
2025-11-03 12:51:00 +0530

    1st Commit

---
+++ src/Scaler_DSA_01/Class_4/Q1.java
@@ -0,0 +1,36 @@
+package Scaler_DSA_01.Class_4;
+
+class Vehicle {
+    int numGears;
+    int numTyres;
+    int topSpeed;
+
+    void startEngine() {
+
+    }
+
+    void stopEngine() {
+
+    }
+}
+
+class cars extends Vehicle {
+    boolean isConvertible;
+
+    void openTrunk() {
+
+    }
+
+}
+
+class bikes extends Vehicle{
+    boolean hasHelmetSpace;
+
+    void kickStart() {
+
+    }
+}
+
+
+public class Q1 {
+}
