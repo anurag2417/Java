ace25e2b9a5db3938d40475fa8cd013e45ae3b1a
Anurag Kumar <anuragakn18@gmail.com>
2025-11-09 14:02:08 +0530

    1st Commit

---
+++ src/GeeksForGeeks__Java/T7_OOP/Q5_Marks.java
@@ -0,0 +1,17 @@
+package GeeksForGeeks__Java.T7_OOP;
+
+public class Q5_Marks {
+    public static class Student_Data{
+        String name;
+        int rollNo;
+        int[] marks;
+
+        Student_Data(int s){
+            marks = new int[s];
+        }
+    }
+    public static void main(String[] args) {
+        Student_Data s1 = new Student_Data(5);
+        Student_Data s2 = new Student_Data(3);
+    }
+}
