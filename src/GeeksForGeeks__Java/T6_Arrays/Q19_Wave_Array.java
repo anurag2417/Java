9674b411ffdb7173dfcccaf7de692bc6e177618a
Anurag Kumar <anuragakn18@gmail.com>
2025-11-16 01:00:09 +0530

    1st Commit

--- src/GeeksForGeeks__Java/T6_Arrays/Q19_Wave_Array.java
+++ src/GeeksForGeeks__Java/T6_Arrays/Q19_Wave_Array.java
@@ -1,4 +1,41 @@
 package GeeksForGeeks__Java.T6_Arrays;
 
+import java.util.*;
+
 public class Q19_Wave_Array {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the Size of the Array : ");
+        int N = sc.nextInt();
+
+        int[] arr = new int[N];
+        System.out.println();
+
+        System.out.println("Enter the Elements of the Array : ");
+        for (int i = 0; i <= arr.length - 1 ; i++) {
+            System.out.print("Element " + (i + 1) + " = ");
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println();
+
+        System.out.print("Elements of the Array : ");
+        for (int i = 0; i <= arr.length - 1; i++){
+            System.out.print(arr[i] + "  ");
+        }
+
+        System.out.println();
+
+        for (int i = 0; i < N; i++) {
+            int temp = arr[i];
+            arr[i] = arr[i+1];
+            arr[i+1] = temp;
+        }
+
+        System.out.print("Elements of the Wavy Array : ");
+        for (int i = 0; i <= arr.length - 1; i++){
+            System.out.print(arr[i] + "  ");
+        }
+    }
 }
