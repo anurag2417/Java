7b4b1f717491ed00fc1f5cc6f0a792f482c35def
Anurag Kumar <anuragakn18@gmail.com>
2025-11-16 02:38:36 +0530

    1st Commit

--- src/GeeksForGeeks__Java/T7_ArrayList/Q1_ArrayList_in_Java_Basics.java
+++ src/GeeksForGeeks__Java/T7_ArrayList/Q1_ArrayList_in_Java_Basics.java
@@ -1,4 +1,33 @@
 package GeeksForGeeks__Java.T7_ArrayList;
 
+import java.util.*;
+
 public class Q1_ArrayList_in_Java_Basics {
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        arr.add(45);
+        arr.add(21);
+        arr.add(23);
+        arr.add(56);
+        arr.add(98);
+
+        // System.out.println(arr[2]);          This will not work here.
+
+        System.out.println(arr.get(2));
+
+        // arr[2] = 56;                         This will not work.
+        // arr.get(2) = 56;                     This will not work.
+
+        arr.set(2, 56);         // same as arr[2] = 56
+        System.out.println(arr.get(2));
+
+        System.out.println(arr);                // This will work in ArrayList.
+                                                // but not traversing the array by ourselves.
+
+        System.out.println();
+        int n = arr.size();     // arr.length
+        for(int i = 0; i < n; i++) {
+            System.out.print(arr.get(i) + " ");
+        }
+    }
 }
