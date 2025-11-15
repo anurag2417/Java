9620c474fc48b0bda3856f8a83c8b4c4db003a6b
Anurag Kumar <anuragakn18@gmail.com>
2025-11-16 03:24:26 +0530

    1st Commit

---
+++ src/GeeksForGeeks__Java/T7_ArrayList/Q3_Add_One.java
@@ -0,0 +1,10 @@
+package GeeksForGeeks__Java.T7_ArrayList;
+
+import java.util.*;
+
+public class Q3_Add_One {
+    public static void main(String[] args) {
+        ArrayList<Integer> ans = new ArrayList<>();
+
+    }
+}
